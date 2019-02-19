5c0600ed1a46a186331e30e8d49ee48dfe4fbcfc
Silvio <silvio.putak@gmail.com>
2019-02-19 20:37:05 +0100

    Povezana baza i napravljena ispravna login forma

---
+++ Akcija/Akcija/Baza_povezivanje/akcija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Akcija.Models;
+using System.Data.Entity;
+
+namespace Akcija.Baza_povezivanje
+{
+    public class akcija :DbContext
+    {
+        public DbSet<Korisnik> korisnik { get; set; }
+    }
+}
--- Akcija/Akcija/Controllers/HomeController.cs
+++ Akcija/Akcija/Controllers/HomeController.cs
@@ -1,30 +1,52 @@
-﻿using System;
+﻿using Akcija.Baza_povezivanje;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Akcija.Models;
 
 namespace Akcija.Controllers
 {
     public class HomeController : Controller
     {
+        private akcija db = new akcija();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        
+
+        [HttpGet]
+        public ActionResult Prijava()
         {
-            ViewBag.Message = "Your application description page.";
+
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        [HttpPost]
 
-            return View();
+        public ActionResult Prijava(Korisnik user)
+        {
+
+            foreach (Korisnik stud in db.korisnik)
+            {
+
+                if (stud.lozinka == user.lozinka && stud.email == user.email)
+                {
+                    
+
+                    
+                    return Content("Logirao si se u sustav");
+                }
+
+
+            }
+
+            return Content("Nisi pravilno unio korisnika");
         }
     }
 }
